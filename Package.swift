1d3d813093b9b148feb7f44434ac8a44827ae462
David Cairns <dcairns@butterflynetinc.com>
2024-11-22 21:58:48 +0000

    Release 1.2.2

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 
 import PackageDescription
 
-// Release: https://github.com/ButterflyNetwork/ImagingSDK-iOS/releases/tag/1.2.1
-let binaryUrl = "https://sdk.butterflynetwork.com/garden/ButterflyImagingKit/1.2.1/ButterflyImagingKit.xcframework.zip"
-let binaryChecksum = "2d3c88f8058b821b162a6364592cc2fd2d94f134209ebe31de9afe2c4fc515d1"
+// Release: https://github.com/ButterflyNetwork/ButterflySDKIOS/releases/tag/1.2.2
+let binaryUrl = "https://sdk.butterflynetwork.com/garden/ButterflyImagingKit/1.2.2/ButterflyImagingKit.xcframework.zip"
+let binaryChecksum = "9726bddcd5566173961bde4e180dae7ac6e32b9fa73bca781cf331a6e53eebd5"
 
 let package = Package(
     name: "Butterfly",
